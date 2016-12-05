691728e1fe8d37c4c81cc2a0d3832c583e2ef30c
Sergii Churaj <sergii.churaj@gmail.com>
2016-12-05 23:30:43 +0200

    Added double checked locked singleton version.

---
+++ Patterns/Patterns/Creational/Singleton/DoubleCheckedLockedSingleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Creational.Singleton
+{
+    public sealed class DoubleCheckedLockedSingleton
+    {
+        private static volatile DoubleCheckedLockedSingleton _instance;
+
+        private static readonly object _syncRoot = new object();
+
+        private DoubleCheckedLockedSingleton()
+        {
+        }
+
+        public static DoubleCheckedLockedSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new DoubleCheckedLockedSingleton();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+        
+        public int Checkpoint { get; set; }
+    }
+}

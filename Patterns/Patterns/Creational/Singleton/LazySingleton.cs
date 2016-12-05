d4ccb94050abdda6712bea8b4028919a6b0740ee
Sergii Churaj <sergii.churaj@gmail.com>
2016-12-05 23:08:29 +0200

    Added lazy singleton version.

---
+++ Patterns/Patterns/Creational/Singleton/LazySingleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Patterns.Creational.Singleton
+{
+    public sealed class LazySingleton
+    {
+        private LazySingleton() { }
+
+        private static readonly Lazy<LazySingleton> _instance = new Lazy<LazySingleton>(() => new LazySingleton());
+
+        public static LazySingleton Instance => _instance.Value;
+
+        public int Checkpoint { get; set; }
+    }
+}
